fce06d4053481c1b7e82a9a01281adb56aa9f8bd
tk20211228 <t3kuboki@gmail.com>
2024-05-04 07:14:57 +0900

    test:vercel deploy test

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
   // basePath: "/",
   images: {
-    unoptimized: true,
+    // unoptimized: true,
   },
 };
 
