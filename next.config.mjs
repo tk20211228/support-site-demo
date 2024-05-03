0fc5b9fbd18ca3901692053ca601c9c4d549cc72
tk20211228 <t3kuboki@gmail.com>
2024-05-03 20:32:41 +0900

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  // basePath: "/",
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    unoptimized: true,
   },
 };
 
