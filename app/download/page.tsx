8de60305912ce3c678e2e5a87939b4970662c4d6
tk20211228 <t3kuboki@gmail.com>
2024-05-05 01:36:12 +0900

    init

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -1,12 +1,10 @@
-import SideBer from "@/components/side-ber";
 import React from "react";
 import { getExternalLinks } from "@/lib/newt";
 import ItemCard from "@/components/item-card";
 
 export default async function Page() {
   const externalLink = await getExternalLinks();
-
-  console.log("externalLink", externalLink);
+  // console.log("externalLink", externalLink);
   return (
     <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {externalLink.map((link) => (
