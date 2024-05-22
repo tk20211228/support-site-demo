bd34ddf8a926d098fd161ae44a479e4d0205f480
tk20211228 <t3kuboki@gmail.com>
2024-05-22 14:02:17 +0900

    feat(page.tsx): layout fixed

--- app/manual/components/manual-card.tsx
+++ app/manual/components/manual-card.tsx
@@ -1,47 +1,35 @@
-import { Search } from "lucide-react";
-
-import { news, newsData, releaseData } from "@/app/data/news";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { manualData } from "@/app/data/manuals";
+import { news, newsData } from "@/app/data/news";
 import NewsCard from "@/components/news-card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function ManualCard() {
   return (
-    <Tabs defaultValue="notice" className="w-full">
-      <TabsList className="grid w-full grid-cols-4">
-        <TabsTrigger value="quickManual">クイックマニュアル</TabsTrigger>
-        <TabsTrigger value="operationManual">操作マニュアル</TabsTrigger>{" "}
-        <TabsTrigger value="manualByFunction">機能別利用の手引き</TabsTrigger>{" "}
-        <TabsTrigger value="systemCommonManual">システム共通資料</TabsTrigger>
-      </TabsList>
-      {news.map((newsItem) => (
-        <TabsContent key={newsItem.id} value={newsItem.id}>
-          <Card>
-            <div className="flex justify-center">
-              <CardHeader>
-                <CardTitle>{newsItem.title}</CardTitle>
-              </CardHeader>
-            </div>
-            <div className="flex justify-center">
-              <CardContent className="space-y-2 lg:w-1/2">
-                <ul className="space-y-2">
-                  {newsData.map((item) => (
-                    <NewsCard
-                      key={item.id}
-                      id={item.id}
-                      title={item.title}
-                      content={item.content}
-                      tag={item.tag}
-                    />
-                  ))}
-                </ul>
-              </CardContent>
-            </div>
-          </Card>
-        </TabsContent>
+    <div className="space-y-2">
+      {manualData.map((data) => (
+        <Card key={data.id}>
+          <div className="flex justify-center">
+            <CardHeader>
+              <CardTitle>{data.title}</CardTitle>
+            </CardHeader>
+          </div>
+          <div className="flex justify-center">
+            <CardContent className="space-y-2 lg:w-1/2">
+              <ul className="space-y-2">
+                {newsData.map((item) => (
+                  <NewsCard
+                    key={item.id}
+                    id={item.id}
+                    title={item.title}
+                    content={item.content}
+                    tag={item.tag}
+                  />
+                ))}
+              </ul>
+            </CardContent>
+          </div>
+        </Card>
       ))}
-    </Tabs>
+    </div>
   );
 }
