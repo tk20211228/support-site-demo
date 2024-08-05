f454944f1c7085e1fcc5b8a5d40ac29db564ed7d
tk20211228 <t3kuboki@gmail.com>
2024-08-06 05:52:00 +0900

    add : 誤字を修正

--- app/data/manuals.ts
+++ app/data/manuals.ts
@@ -33,85 +33,82 @@ export const manualCategory: Manual[] = [
 export const manualData: Manual[] = [
   {
     id: 1,
-    title:
-      "クイックスタートガイド 利用者編 Android エージェント (インストール編)",
+    title: "クイックスタートガイド Android ",
     url: "https://www.google.com",
   },
   {
     id: 2,
-    title: "クイックスタートガイド 利用者編 iOS エージェント (インストール編)",
+    title: "クイックスタートガイド iOS ",
     url: "https://www.google.com",
   },
   {
     id: 3,
-    title:
-      "クイックスタートガイド 利用者編 macOS エージェント (インストール編)",
+    title: "クイックスタートガイド macOS ",
     url: "https://www.google.com",
   },
   {
     id: 4,
-    title:
-      "クイックスタートガイド 利用者編 Windows エージェント (インストール編)",
+    title: "クイックスタートガイド Windows ",
     url: "https://www.google.com",
   },
   {
     id: 5,
-    title: "操作マニュアル Android エージェント (インストール編)",
+    title: "操作マニュアル Android ",
     url: "https://www.google.com",
   },
   {
     id: 6,
-    title: "操作マニュアル iOS エージェント (インストール編)",
+    title: "操作マニュアル iOS ",
     url: "https://www.google.com",
   },
   {
     id: 7,
-    title: "操作マニュアル macOS エージェント (インストール編)",
+    title: "操作マニュアル macOS ",
     url: "https://www.google.com",
   },
   {
     id: 8,
-    title: "操作マニュアル Windows エージェント (インストール編)",
+    title: "操作マニュアル Windows ",
     url: "https://www.google.com",
   },
   {
     id: 9,
-    title: "機能別マニュアル Android エージェント (インストール編)",
+    title: "機能別マニュアル Android ",
     url: "https://www.google.com",
   },
   {
     id: 10,
-    title: "機能別マニュアル iOS エージェント (インストール編)",
+    title: "機能別マニュアル iOS ",
     url: "https://www.google.com",
   },
   {
     id: 11,
-    title: "機能別マニュアル macOS エージェント (インストール編)",
+    title: "機能別マニュアル macOS ",
     url: "https://www.google.com",
   },
   {
     id: 12,
-    title: "機能別マニュアル Windows エージェント (インストール編)",
+    title: "機能別マニュアル Windows ",
     url: "https://www.google.com",
   },
   {
     id: 13,
-    title: "システム共通資料  Android エージェント (インストール編)",
+    title: "システム共通資料  Android ",
     url: "https://www.google.com",
   },
   {
     id: 14,
-    title: "システム共通資料  iOS エージェント (インストール編)",
+    title: "システム共通資料  iOS ",
     url: "https://www.google.com",
   },
   {
     id: 15,
-    title: "システム共通資料  macOS エージェント (インストール編)",
+    title: "システム共通資料  macOS ",
     url: "https://www.google.com",
   },
   {
     id: 16,
-    title: "システム共通資料  Windows エージェント (インストール編)",
+    title: "システム共通資料  Windows ",
     url: "https://www.google.com",
   },
 ];
